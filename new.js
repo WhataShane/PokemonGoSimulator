054e8b8c1af9fadda7deaffb2d49b119662d47a9
WhataShane <sb@Shanes-Air.attlocal.net>
2016-08-19 06:03:23 -0700

    Huge Overhaul - Deleted iframe, condensed Javascript

--- new.js
+++ new.js
@@ -1,24 +1,3 @@
-$(document).ready(function() {
-    $(".pokemon").click(function() {
-      $(this).addClass('animation');
-	setTimeout( function() {
-		$('.pokemon').removeClass('animation');
-	}, 400);
-        document.getElementById('game').contentWindow.location.reload(true);
-    });
-    
-    
-;
-
-
-jQuery(document).ready(function($){
-    $('#game').iframeTracker({
-        blurCallback: function(){
-            $('.container2').fadeOut()
-            $('button').fadeOut()
-        }
-    });
+$( document ).on( 'click', 'canvas', function() {
+   $('.container2').fadeOut()
 });
-
-});
-
